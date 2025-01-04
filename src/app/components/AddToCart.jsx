141b7644fa3ce9b5e2ffa4d4533c05d1466ead0f
Deelolade <Oluwanisholahabeeb@icloud.com>
2025-01-04 22:50:27 +0100

    first commit

---
+++ src/app/components/AddToCart.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+const AddToCart = () => {
+    return (
+        <div>
+            <button onClick={() => { console.log("hello world") }}>click me</button>
+
+        </div>
+    )
+}
+
+export default AddToCart
