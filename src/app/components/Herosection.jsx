c8054792720f839ecd3e7607675126c321eb7a13
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-09-29 16:58:55 +0100

    progressing --2

--- src/app/components/Herosection.jsx
+++ src/app/components/Herosection.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
 import imageOne from "../../../public/images/Herosection-one.webp";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -8,95 +8,71 @@ gsap.registerPlugin(ScrollTrigger);
 import { useGSAP } from "@gsap/react";
 
 const Herosection = () => {
- const heroText = useRef();
- const heroImage = useRef();
+    const heroText = useRef(null);
+    const heroImage = useRef(null);
 
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: heroText.current,
-    start: "top center",
-    end: "bottom center",
-    scrub: true,
-    markers: true,
-    pin: true,
-    pinSpacing: true,
-  },
-});
+    useGSAP(()=>{
+        const tl = gsap.timeline({
+            scrollTrigger:{
+                trigger: heroImage.current,
+                start: ' top 50%',
+                end: ' 40% center',
+                markers: true,
+            }
+        })
+        let ctx = gsap.context(()=>{
+            gsap.set(heroText.current,{
+                position: 'fixed'
+            })
 
-useGSAP(() => {
-  let ctx = gsap.context(() => {
-    // Keep hero-text centered & fixed
-    gsap.set(".hero-text", {
-      position: "fixed",
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-    });
-
-    // Animate image-one
-    tl
-    .from(".image-one", {
-      scale: 1,
-      ease: "none",
-      duration: 1,
+            tl.to(heroImage.current,{
+                scale:1.5
+            })
+            .to(heroText.current,{
+                color: 'white'
+            })
+        })
+        return ()=> ctx.revert();
     })
-    .to(".image-one", {
-    //   scale: 1.8,
-      ease: "none",
-      duration: 1,
-    }, '<')
-    .to(heroText.current,{
-        y:-900,
-      ease: "none",
 
-    }, '<')
-     .to(heroText.current.querySelectorAll( 'h1, h3, p'), {
-        y:-300,
-      color: "#ffffff", // change to white (or any color you want)
-    //   duration: 1,
-      ease: "none",
-  }, '<');
-
-});
-  return () => ctx.revert(); // cleanup
-})
-  return (
-    <>
-      <main>
-        <section className="min-h-screen flex justify-center items-center ">
-          <div className="max-w-6xl gap-0 h-[316px] hero-text z-40 bg-red-200" ref={heroText}>
-            <h1 className="text-[150px] font-semibold text-black leading-[120px]">
-              Work. Life.
-            </h1>
-            <div className="relative">
-              <h3 className=" absolute right-[-8rem] text-7xl font-normal text-right text-black">
-                Redefined
-              </h3>
-            </div>
-            <div className="mt-20">
-              <p className="text-black font-semibold text-lg text-center">
-                Workspaces your team looks forward to. <br />
-                Lifestyle venues that capture the essence of the future.
-              </p>
-            </div>
-          </div>
-        </section>
-        <section className="  flex justify-center items-start ">
-          <div className="max-w-fit">
-            <Image
-              src={imageOne}
-                alt="image-one"
-                ref={heroImage}
-              className="image-one max-w-[100vw] bg-blue-400 w-screen h-[440px] object-cover"
-            />
-          </div>
-        </section>
-        <section className="min-h-screen flex justify-center items-center ">
-
-        </section>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main>
+                <section className="min-h-screen flex justify-center items-center ">
+                    <div
+                        className="max-w-6xl gap-0 h-[316px] mt-28 hero-text z-40 bg-red-200"
+                        ref={heroText}
+                    >
+                        <h1 className="text-[150px] font-semibold text-black leading-[120px]">
+                            Work. Life.
+                        </h1>
+                        <div className="relative">
+                            <h3 className=" absolute right-[-8rem] text-7xl font-normal text-right text-black">
+                                Redefined
+                            </h3>
+                        </div>
+                        <div className="mt-20">
+                            <p className="text-black font-semibold text-lg text-center">
+                                Workspaces your team looks forward to. <br />
+                                Lifestyle venues that capture the essence of the future.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                <section className="min-h-screen  flex justify-center items-start ">
+                    <div className="max-w-fit">
+                        <Image
+                            src={imageOne}
+                            alt="image-one"
+                            ref={heroImage}
+                            className="image-one max-w-[100vw] bg-blue-400 w-screen h-[440px] object-cover"
+                        />
+                    </div>
+                </section>
+                <section className="min-h-screen  flex justify-center items-start "></section>
+            </main>
+        </>
+    );
 };
 
 export default Herosection;
